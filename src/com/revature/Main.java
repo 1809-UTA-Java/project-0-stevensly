88c0bfd49a53faf821241e131e86f5e6b187009c
stevensly <steven.f.sly@protonmail.com>
2018-09-27 15:55:05 -0500

    Initial commit.  Added the Main class.

---
+++ src/com/revature/Main.java
@@ -0,0 +1,10 @@
+package com.revature;
+
+/**
+ * This is the Main class for Project 0, a banking application.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("this is the main method.");
+    }
+}
